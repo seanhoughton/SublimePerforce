c4b6fc4cb3bb5a2acc1aa8a028f8c12b5d0f3814
Sean Houghton <sean.houghton@gmail.com>
2011-10-17 09:58:26 -0700

    Added README.  Removed more debug logging

--- perforce.py
+++ perforce.py
@@ -5,11 +5,9 @@ import os, stat
 class PerforceCommand(sublime_plugin.EventListener):
 	def on_modified(self, view):
 		fileToCheckout = view.file_name()
-		print fileToCheckout
 		if fileToCheckout != None:
 			fileMode = os.stat(fileToCheckout)[stat.ST_MODE]
 			if (not fileMode & stat.S_IWRITE):
-				#print "Attempting to check out " + fileToCheckout
 				cmd = "p4 edit " + fileToCheckout
 				output = os.popen(cmd).read()
 				print output;
