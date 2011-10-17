8e34d921dbe47b4327e18d86cfee6751af4d3b89
unknown <shoughton@.pd.scea.com>
2011-10-17 09:39:41 -0700

    Initial checkin.  Working version.

---
+++ perforce.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin
+import os, stat
+
+
+class PerforceCommand(sublime_plugin.EventListener):
+	def on_modified(self, view):
+		fileToCheckout = view.file_name()
+		print fileToCheckout
+		if fileToCheckout != None:
+			fileMode = os.stat(fileToCheckout)[stat.ST_MODE]
+			if (not fileMode & stat.S_IWRITE):
+				print "Attempting to check out " + fileToCheckout
+				cmd = "p4 edit " + fileToCheckout
+				output = os.popen(cmd).read()
+				#os.system(cmd)
+				print output;
